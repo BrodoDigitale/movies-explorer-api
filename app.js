362e3b04ae0c5ad8a5c73ab9d57bec86e0ee1581
Julia Sensi <sensi.julia@gmail.com>
2022-01-12 14:51:57 +0100

    fix: change PORT

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const moviesRoutes = require('./routes/movies');
 const NotFoundError = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 app.use(cookieParser());
